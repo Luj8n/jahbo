ac5a64fe3812134f13904e8a2be54e9e3764a924
Luj8n <l.milieska@gmail.com>
2022-03-11 23:51:51 +0200

    added icon

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,18 @@ fn get_toml_value(file_name: &str, key: &str) -> toml::Value {
 
 fn main() {
   let app = app::App::default();
+
+  let icon_bytes = include_bytes!("../assets/icon.png");
+  let icon = image::load_from_memory(icon_bytes).unwrap();
+
   let native_options = eframe::NativeOptions {
     maximized: true,
+    // transparent: true, // TODO
+    icon_data: Some(eframe::epi::IconData {
+      width: icon.width(),
+      height: icon.height(),
+      rgba: icon.into_bytes(),
+    }),
     ..Default::default()
   };
   eframe::run_native(Box::new(app), native_options);
